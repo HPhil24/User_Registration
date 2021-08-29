ea7cfd86b81e8ed8d44b7e8afb12641f79f6707e
Philip Hagos <philhagos676@gmail.com>
2021-08-29 18:52:59 +0800

    Updated UsersList.js

--- user-registration/src/CreateUser.js
+++ user-registration/src/CreateUser.js
@@ -1,7 +1,61 @@
-import React from "react"
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
 export const CreateUser = () => {
+  const { register, handleSubmit } = useForm();
+  const history = useHistory()
 
-    return <div className="container">Create User Account</div>
-
-}
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+    history.push("/")
+  });
+  return (
+    <div className="container">
+      <div className="mt-5">
+        <h3>
+          <center>User Registration</center>
+        </h3>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="text">Name:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              id="name"
+              autoComplete="off"
+              {...register("text", { required: "Required" })}
+            />
+            <br></br>
+            <label htmlFor="text">Email:</label>
+            <input
+              className="form-control"
+              type="email"
+              name="description"
+              id="description"
+              autoComplete="off"
+              {...register("description", { required: "Required" })}
+            />
+            <br></br>
+            <label htmlFor="text">Address:</label>
+            <input
+              className="form-control"
+              type="text"
+              name="address"
+              id="address"
+              autoComplete="off"
+              {...register("address", { required: "Required" })}
+            />
+            <br></br>
+          </div> 
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- user-registration/src/UsersList.js
+++ user-registration/src/UsersList.js
@@ -1,7 +1,53 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const UsersList = () => {
+  const [items, setItems] = useState([]);
 
-    return <div className="container">Users List</div>
+  useEffect(() => {
+    setItems([
+        { name: "Phil", id: 0 },
+        { name: "Hagos", id: 1 },
+    ]);
+  }, []);
 
-}
+  return (
+    <div className="container">
+      <div className="mt-5">
+        <h3>
+          <center>Registered Users</center>
+        </h3>
+        <table className="table table-striped table-hover table-responsive table-dark">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            items.map(user => (
+              <tr key={user.id}>
+                <td>
+                  {user.name}
+                </td>
+                <td>
+                  {user.email}
+                </td>
+                <td>
+                  {user.address}
+                </td>
+                <td>
+                  <Link to={`/edit/${user.id}`}>Edit</Link>
+                </td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
